1c65b78291266e223d612ad3f85d1cb242852ca7
thacbaonguyen <ar.thbaonguyen@gmail.com>
2024-01-22 07:32:38 +0700

    createdb

--- src/main/java/DiamonShop/Controller/User/HomeController.java
+++ src/main/java/DiamonShop/Controller/User/HomeController.java
@@ -1,9 +1,13 @@
-package DiamonShop.UserController;
+package DiamonShop.Controller.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import DiamonShop.Dao.SlidesDao;
+import DiamonShop.Service.User.HomeServiceIpl;
 
 @Controller
 //public class HomeController {
@@ -12,10 +16,14 @@ import org.springframework.web.servlet.ModelAndView;
 //		return "user/index";
 //	}
 //}
+
 public class HomeController {
+	@Autowired
+	HomeServiceIpl HomeService;
 	@RequestMapping(value = {"/", "/trang-chu"}, method = RequestMethod.GET)
 	public ModelAndView Index() {
 		ModelAndView mv = new ModelAndView("user/index");
+		mv.addObject("slides",HomeService.GetDataSlide());
 		return mv;
 	}
 	@RequestMapping(value = "/product")
